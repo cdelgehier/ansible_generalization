5a4c341f4ef3c6c30e190d64f3f8454c13e3064f
Delgehier Cédric <cedric.delgehier@gmail.com>
2018-05-20 00:01:21 +0000

    WIP

--- tools/spawn_env.sh
+++ tools/spawn_env.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
-docker run --detach --privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --volume=$HOME/.ssh:/root/.ssh centos_sshd /usr/lib/systemd/systemd > "/tmp/172.17.0.2"
-docker run --detach --privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --volume=$HOME/.ssh:/root/.ssh centos_sshd /usr/lib/systemd/systemd > "/tmp/172.17.0.3"
-docker run --detach --privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --volume=$HOME/.ssh:/root/.ssh centos_sshd /usr/lib/systemd/systemd > "/tmp/172.17.0.4"
-sleep 1
-docker run --detach --volume=/run --volume=/run/lock --volume=/tmp --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --cap-add=SYS_ADMIN --cap-add=SYS_RESOURCE --volume=$HOME/.ssh:/root/.ssh ubuntu_sshd /sbin/init > "/tmp/172.17.0.5"
-docker run --detach --volume=/run --volume=/run/lock --volume=/tmp --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --cap-add=SYS_ADMIN --cap-add=SYS_RESOURCE --volume=$HOME/.ssh:/root/.ssh ubuntu_sshd /sbin/init > "/tmp/172.17.0.6"
-docker run --detach --volume=/run --volume=/run/lock --volume=/tmp --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --cap-add=SYS_ADMIN --cap-add=SYS_RESOURCE --volume=$HOME/.ssh:/root/.ssh ubuntu_sshd /sbin/init > "/tmp/172.17.0.7"
+for i in $(seq 1 6); do
+docker run --detach --privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --volume=$HOME/.ssh:/root/.ssh --name node${i} --hostname node${i} centos_sshd /usr/lib/systemd/systemd > "/tmp/172.17.0.$((i+1))"
+done
+#sleep 1
+#docker run --detach --volume=/run --volume=/run/lock --volume=/tmp --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --cap-add=SYS_ADMIN --cap-add=SYS_RESOURCE --volume=$HOME/.ssh:/root/.ssh ubuntu_sshd /sbin/init > "/tmp/172.17.0.5"
+#docker run --detach --volume=/run --volume=/run/lock --volume=/tmp --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --cap-add=SYS_ADMIN --cap-add=SYS_RESOURCE --volume=$HOME/.ssh:/root/.ssh ubuntu_sshd /sbin/init > "/tmp/172.17.0.6"
+#docker run --detach --volume=/run --volume=/run/lock --volume=/tmp --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro --cap-add=SYS_ADMIN --cap-add=SYS_RESOURCE --volume=$HOME/.ssh:/root/.ssh ubuntu_sshd /sbin/init > "/tmp/172.17.0.7"
 
